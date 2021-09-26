6b5fe411484a0c5b9a634de837708d63ade11733
Matthew <justmatt@outlook.com>
2021-09-26 17:58:55 -0500

    Refactor Migration code

---
+++ XSLite/Migrations/JsonAsset/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿namespace XSLite.Migrations.JsonAsset
+{
+    internal record Ingredient
+    {
+        public int Object { get; set; }
+
+        public int Count { get; set; } = 1;
+    }
+}
---
+++ XSLite/Migrations/JsonAsset/JsonAsset.cs
@@ -0,0 +1,23 @@
+﻿namespace XSLite.Migrations.JsonAsset
+{
+    using System.Collections.Generic;
+
+    internal record JsonAsset
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? ReserveExtraIndexCount { get; set; } = 0;
+
+        public Recipe? Recipe { get; set; } = null;
+
+        public string? PurchaseFrom { get; set; } = null;
+
+        public int? PurchasePrice { get; set; } = 0;
+
+        public Dictionary<string, string>? NameLocalization { get; set; } = null;
+
+        public Dictionary<string, string>? DescriptionLocalization { get; set; } = null;
+    }
+}
