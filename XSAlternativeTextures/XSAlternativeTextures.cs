3f68569db71db3d78e9665c9829f6237451c7b04
Matthew <justmatt@outlook.com>
2021-09-26 17:58:08 -0500

    Updated to match assetName from AlternativeTextures

--- XSAlternativeTextures/XSAlternativeTextures.cs
+++ XSAlternativeTextures/XSAlternativeTextures.cs
@@ -14,31 +14,31 @@
     /// <inheritdoc cref="StardewModdingAPI.Mod" />
     public class XSAlternativeTextures : Mod, IAssetEditor
     {
-        private readonly IList<string> Storages = new List<string>();
-        private IAlternativeTexturesAPI AlternativeTexturesAPI;
-        private XSLiteIntegration XSLite;
+        private readonly IList<string> _storages = new List<string>();
+        private IAlternativeTexturesAPI _alternativeTexturesAPI = null!;
+        private XSLiteIntegration _xsLite = null!;
 
         /// <inheritdoc />
         public override void Entry(IModHelper helper)
         {
-            this.XSLite = new XSLiteIntegration(helper.ModRegistry);
+            this._xsLite = new XSLiteIntegration(helper.ModRegistry);
             this.Helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
         }
 
         /// <inheritdoc />
         public bool CanEdit<T>(IAssetInfo asset)
         {
-            return asset.AssetNameEquals("AlternativeTextures/Textures/ExpandedStorage.Craftable_Chest");
+            return asset.AssetName.StartsWith("AlternativeTextures") && asset.AssetName.Contains("ExpandedStorage");
         }
 
         /// <inheritdoc />
         public void Edit<T>(IAssetData asset)
         {
             IAssetDataForImage editor = asset.AsImage();
-            editor.ExtendImage(80, this.Storages.Count * 32);
-            for (int i = 0; i < this.Storages.Count; i++)
+            editor.ExtendImage(80, this._storages.Count * 32);
+            for (int i = 0; i < this._storages.Count; i++)
             {
-                var texture = this.Helper.Content.Load<Texture2D>($"ExpandedStorage/SpriteSheets/{this.Storages[i]}", ContentSource.GameContent);
+                var texture = this.Helper.Content.Load<Texture2D>($"ExpandedStorage/SpriteSheets/{this._storages[i]}", ContentSource.GameContent);
                 editor.PatchImage(texture, new Rectangle(0, 0, 16, 32), new Rectangle(0,  i * 32, 16, 32));
                 editor.PatchImage(texture, new Rectangle(0, 0, 80, 32), new Rectangle(16,  i * 32, 80, 32));
             }
@@ -46,21 +46,21 @@
 
         private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
         {
-            this.AlternativeTexturesAPI = this.Helper.ModRegistry.GetApi<IAlternativeTexturesAPI>("PeacefulEnd.AlternativeTextures");
+            this._alternativeTexturesAPI = this.Helper.ModRegistry.GetApi<IAlternativeTexturesAPI>("PeacefulEnd.AlternativeTextures");
             var model = new AlternativeTextureModel
             {
                 ItemName = "Chest",
                 Type = "Craftable",
                 TextureWidth = 16,
                 TextureHeight = 32,
-                Variations = this.Storages.Count,
+                Variations = this._storages.Count,
                 EnableContentPatcherCheck = true,
             };
             var textures = new List<Texture2D>();
-            var placeholder = this.Helper.Content.Load<Texture2D>("assets/texture.png");
-            foreach (string storageName in this.XSLite.API.GetAllStorages().OrderBy(storageName => storageName))
+            Texture2D placeholder = this.Helper.Content.Load<Texture2D>("assets/texture.png");
+            foreach (string storageName in this._xsLite.API.GetAllStorages().OrderBy(storageName => storageName))
             {
-                Texture2D texture = null;
+                Texture2D texture = null!;
                 try
                 {
                     texture = this.Helper.Content.Load<Texture2D>($"ExpandedStorage/SpriteSheets/{storageName}", ContentSource.GameContent);
@@ -76,15 +76,15 @@
                 }
 
                 textures.Add(placeholder);
-                this.Storages.Add(storageName);
+                this._storages.Add(storageName);
                 model.ManualVariations.Add(new VariationModel
                 {
-                    Id = this.Storages.Count - 1,
+                    Id = this._storages.Count - 1,
                     Keywords = new List<string> { storageName },
                 });
             }
 
-            this.AlternativeTexturesAPI.AddAlternativeTexture(model, "ExpandedStorage", textures);
+            this._alternativeTexturesAPI.AddAlternativeTexture(model, "ExpandedStorage", textures);
         }
     }
 }
