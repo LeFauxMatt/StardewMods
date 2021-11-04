be50e0bea928e5e6122f0c17c3480192fa6ce280
Matthew <justmatt@outlook.com>
2021-11-03 22:16:05 -0500

    Fixes tabs being pressed by any button

--- XSPlus/Features/InventoryTabsFeature.cs
+++ XSPlus/Features/InventoryTabsFeature.cs
@@ -178,7 +178,7 @@
                 return;
             }
 
-            if (e.Button != SButton.MouseLeft && e.IsDown(SButton.MouseLeft))
+            if (e.Button != SButton.MouseLeft && !e.IsDown(SButton.MouseLeft))
             {
                 return;
             }
