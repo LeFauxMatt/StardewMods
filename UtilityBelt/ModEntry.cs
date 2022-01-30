4db3dbf12f8501691baba6dd6580f8c002babce0
Matthew <justmatt@outlook.com>
2022-01-30 01:06:51 -0600

    Initial UtilityBelt

---
+++ UtilityBelt/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace UtilityBelt;
+
+using StardewModdingAPI;
+
+/// <inheritdoc />
+public class ModEntry : Mod
+{
+    /// <inheritdoc/>
+    public override void Entry(IModHelper helper)
+    {
+        
+    }
+}
