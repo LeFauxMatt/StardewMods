ec2aa2d1067cb8d5eb8197ab288b5fccd881969f
Matthew Lucas <justmatt@outlook.com>
2022-08-07 13:04:30 -0700

    Added enum for configurable color options

---
+++ Common/Enums/Colors.cs
@@ -0,0 +1,32 @@
+﻿namespace StardewMods.Common.Enums;
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Xna.Framework;
+using NetEscapades.EnumGenerators;
+
+/// <summary>
+///     Enum representation of some <see cref="Color" />.
+/// </summary>
+[EnumExtensions]
+[SuppressMessage("StyleCop", "SA1602", Justification = "No")]
+public enum Colors
+{
+    Red,
+    DarkRed,
+    PaleVioletRed,
+    Blue,
+    Green,
+    DarkGreen,
+    Jade,
+    Brown,
+    DarkBrown,
+    Yellow,
+    DarkYellow,
+    Aquamarine,
+    Purple,
+    DarkPurple,
+    Cyan,
+    Pink,
+    Orange,
+    Gray,
+}
