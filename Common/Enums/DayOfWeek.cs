3e2df61b2c6322025522677d4f586b0f11873a54
Matthew Lucas <justmatt@outlook.com>
2022-08-19 18:40:39 -0700

    Added DayOfWeek Enum

---
+++ Common/Enums/DayOfWeek.cs
@@ -0,0 +1,31 @@
+﻿namespace StardewMods.Common.Enums;
+
+using NetEscapades.EnumGenerators;
+
+/// <summary>
+///     The day of the week.
+/// </summary>
+[EnumExtensions]
+public enum DayOfWeek
+{
+    /// <summary>The first day of the week.</summary>
+    Monday = 0,
+
+    /// <summary>The second day of the week.</summary>
+    Tuesday = 1,
+
+    /// <summary>The third day of the week.</summary>
+    Wednesday = 2,
+
+    /// <summary>The fourth day of the week.</summary>
+    Thursday = 3,
+
+    /// <summary>The fifth day of the week.</summary>
+    Friday = 4,
+
+    /// <summary>The sixth day of the week.</summary>
+    Saturday = 5,
+
+    /// <summary>The seventh day of the week.</summary>
+    Sunday = 6,
+}
