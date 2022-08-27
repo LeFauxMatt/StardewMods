38489cc0eb0910e77d449bd2b2b315bcf53b55e6
Matthew Lucas <justmatt@outlook.com>
2022-08-27 15:33:49 -0700

    Added Integrations for Project Fluent and Content Patcher

---
+++ Common/Integrations/ContentPatcher/ContentPatcherIntegration.cs
@@ -0,0 +1,18 @@
+﻿namespace StardewMods.Common.Integrations.ContentPatcher;
+
+/// <inheritdoc />
+internal class ContentPatcherIntegration : ModIntegration<IContentPatcherApi>
+{
+    private const string ModUniqueId = "Pathoschild.ContentPatcher";
+    private const string ModVersion = "1.28.0";
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ContentPatcherIntegration"/> class.
+    /// </summary>
+    /// <param name="modRegistry">SMAPI's mod registry.</param>
+    public ContentPatcherIntegration(IModRegistry modRegistry)
+        : base(modRegistry, ContentPatcherIntegration.ModUniqueId, ContentPatcherIntegration.ModVersion)
+    {
+        // Nothing
+    }
+}
---
+++ Common/Integrations/ProjectFluent/IProjectFluentApi.cs
@@ -0,0 +1,14 @@
+﻿namespace StardewMods.Common.Integrations.ProjectFluent;
+
+/// <summary>The Project Fluent API which other mods can access.</summary>
+public interface IProjectFluentApi
+{
+    /// <summary>
+    ///     Get an <see cref="IFluent{}" /> instance that allows retrieving Project Fluent translations for the current
+    ///     locale.
+    /// </summary>
+    /// <remarks>The returned instance's locale will change automatically if the <see cref="CurrentLocale" /> changes.</remarks>
+    /// <param name="mod">The mod for which to retrieve the translations.</param>
+    /// <param name="file">An optional file name to retrieve the translations from.</param>
+    IFluent<string> GetLocalizationsForCurrentLocale(IManifest mod, string? file = null);
+}
