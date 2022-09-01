bae43e8a5ca09b497fb3fe5a04f41d2a77005cb8
Matthew Lucas <justmatt@outlook.com>
2022-09-01 00:04:11 -0700

    Toggle fallback for CursorTile when out of range

--- Common/Helpers/CommonHelpers.cs
+++ Common/Helpers/CommonHelpers.cs
@@ -62,8 +62,9 @@ internal static class CommonHelpers
     ///     Gets the map tile the cursor is over.
     /// </summary>
     /// <param name="radius">The tile distance from the player.</param>
+    /// <param name="fallback">Fallback to grab tile if cursor tile is out of range.</param>
     /// <returns>Returns the tile position.</returns>
-    public static Vector2 GetCursorTile(int radius = 0)
+    public static Vector2 GetCursorTile(int radius = 0, bool fallback = true)
     {
         if (radius == 0)
         {
@@ -74,7 +75,7 @@ internal static class CommonHelpers
         pos.X = (int)pos.X;
         pos.Y = (int)pos.Y;
 
-        if (!Utility.tileWithinRadiusOfPlayer((int)pos.X, (int)pos.Y, radius, Game1.player))
+        if (fallback && !Utility.tileWithinRadiusOfPlayer((int)pos.X, (int)pos.Y, radius, Game1.player))
         {
             pos = Game1.player.GetGrabTile();
         }
