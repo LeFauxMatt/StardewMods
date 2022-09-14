a08bf5071b66fba5ae9f002610e6c26af20e3afc
Matthew Lucas <justmatt@outlook.com>
2022-09-13 21:56:37 -0700

    Added ShoppingCart Api.

---
+++ Common/Integrations/ShoppingCart/IShoppingCartApi.cs
@@ -0,0 +1,12 @@
+﻿namespace StardewMods.Common.Integrations.ShoppingCart;
+
+/// <summary>
+///     API for Shopping Cart.
+/// </summary>
+public interface IShoppingCartApi
+{
+    /// <summary>
+    ///     Gets the current shop.
+    /// </summary>
+    IShop? CurrentShop { get; }
+}
---
+++ ShoppingCart/ShoppingCartApi.cs
@@ -0,0 +1,10 @@
+﻿namespace StardewMods.ShoppingCart;
+
+using StardewMods.Common.Integrations.ShoppingCart;
+
+/// <inheritdoc />
+public sealed class ShoppingCartApi : IShoppingCartApi
+{
+    /// <inheritdoc />
+    public IShop? CurrentShop => ShoppingCart.CurrentShop;
+}
