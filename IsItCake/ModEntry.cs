8a2527d331d8dd32bbe2a82d06c1462ea7c097ee
FauxMatt <github@fauxmail.net>
2022-12-15 08:17:34 -0800

    feat:
    added IsItCake project

---
+++ IsItCake/ModEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace StardewMods.IsItCake;
+
+/// <inheritdoc />
+public sealed class ModEntry : Mod
+{
+    /// <inheritdoc/>
+    public override void Entry(IModHelper helper)
+    {
+        throw new System.NotImplementedException();
+    }
+}
