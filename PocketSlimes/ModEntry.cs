1487bd9d60814ccd260484b0787c122bd7f2392f
FauxMatt <github@fauxmail.net>
2023-02-15 20:11:02 -0800

    feat: Pocket Slimes mod

---
+++ PocketSlimes/ModEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace StardewMods.PocketSlimes;
+
+using StardewMods.Common.Helpers;
+using StardewMods.PocketSlimes.Framework;
+
+/// <inheritdoc />
+public sealed class ModEntry : Mod
+{
+    /// <inheritdoc />
+    public override void Entry(IModHelper helper)
+    {
+        // Init
+        Log.Monitor = this.Monitor;
+        ModPatches.Init(this.ModManifest);
+    }
+}
