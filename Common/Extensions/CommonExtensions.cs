4c919686780bbcf9d2cd19c87296e0a6d9ee8479
FauxMatt <github@fauxmail.net>
2023-12-08 01:05:53 -0800

    Updated InvokeAll to fix null EventArgs

--- Common/Extensions/CommonExtensions.cs
+++ Common/Extensions/CommonExtensions.cs
@@ -59,7 +59,7 @@ internal static class CommonExtensions
         {
             try
             {
-                handler.DynamicInvoke(source);
+                handler.DynamicInvoke(source, null);
             }
             catch (Exception)
             {
