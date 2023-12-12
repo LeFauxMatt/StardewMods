ecb93d6e70ca2c59a1f674392973d5f3c06e0bf7
FauxMatt <github@fauxmail.net>
2023-12-11 23:55:26 -0800

    Added GMCM integration

---
+++ FuryCore/ModConfig.cs
@@ -0,0 +1,11 @@
+namespace StardewMods.FuryCore;
+
+using StardewMods.Common.Enums;
+using StardewMods.Common.Interfaces;
+
+/// <summary>Mod config data for Custom Bush.</summary>
+internal sealed class ModConfig : IConfigWithLogLevel
+{
+    /// <inheritdoc />
+    public LogLevels LogLevel { get; set; } = LogLevels.Less;
+}
--- FuryCore/ModEntry.cs
+++ FuryCore/ModEntry.cs
@@ -1,6 +1,10 @@
 namespace StardewMods.FuryCore;
 
 using SimpleInjector;
+using StardewModdingAPI.Events;
+using StardewMods.Common.Helpers;
+using StardewMods.Common.Integrations.GenericModConfigMenu;
+using StardewMods.Common.Interfaces;
 using StardewMods.FuryCore.Framework;
 
 /// <inheritdoc />
@@ -11,8 +15,46 @@ public sealed class ModEntry : Mod
 #nullable enable
 
     /// <inheritdoc />
-    public override void Entry(IModHelper helper) => this.container = new();
+    public override void Entry(IModHelper helper)
+    {
+        // Init
+        I18n.Init(this.Helper.Translation);
+
+        // Events
+        this.Helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+    }
 
     /// <inheritdoc />
     public override object GetApi(IModInfo mod) => new FuryCoreApi();
+
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        var config = this.Helper.ReadConfig<ModConfig>();
+
+        // Init
+        this.container = new();
+        this.container.RegisterInstance(config);
+        this.container.RegisterInstance<IConfigWithLogLevel>(config);
+
+        // SMAPI
+        this.container.RegisterInstance(this.Helper);
+        this.container.RegisterInstance(this.ModManifest);
+        this.container.RegisterInstance(this.Monitor);
+        this.container.RegisterInstance(this.Helper.Data);
+        this.container.RegisterInstance(this.Helper.Events);
+        this.container.RegisterInstance(this.Helper.GameContent);
+        this.container.RegisterInstance(this.Helper.Input);
+        this.container.RegisterInstance(this.Helper.ModContent);
+        this.container.RegisterInstance(this.Helper.ModRegistry);
+        this.container.RegisterInstance(this.Helper.Reflection);
+        this.container.RegisterInstance(this.Helper.Translation);
+
+        // Integrations
+        this.container.RegisterSingleton<GenericModConfigMenuIntegration>();
+
+        // Services
+        this.container.RegisterSingleton<Logging>();
+
+        this.container.Verify();
+    }
 }
