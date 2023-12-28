f434d348c477d5e3b52dfdbac67baafe7943fc3e
FauxMatt <github@fauxmail.net>
2023-12-28 01:30:09 -0800

    Correct typo in IntegrationType

--- ToolbarIcons/Framework/Enums/IntegrationType.cs
+++ ToolbarIcons/Framework/Enums/IntegrationType.cs
@@ -7,7 +7,7 @@ using StardewValley.Menus;
 [EnumExtensions]
 internal enum IntegrationType
 {
-    /// <summary>Opens an <see cref="IClickableMenu" /> from the mod..</summary>
+    /// <summary>Opens an <see cref="IClickableMenu" /> from the mod.</summary>
     Menu,
 
     /// <summary>Invokes a method from the mod.</summary>
